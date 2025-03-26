19f7d376454499191ed45d7f4390a1757f455027
Elliott Wagner <elliottjwag@hotmail.com>
2025-03-26 14:20:34 -0600

    Add project files.

---
+++ WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (CityLabel.Text != "")
+            {
+                CityLabel.Text = SearchEntry.Text;
+            }
+        }
+    }
+
+}
