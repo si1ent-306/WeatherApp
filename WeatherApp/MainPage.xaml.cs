0e8ff7d52d069395d132e3d2b81c41db53e3b5fa
Elliott Wagner <elliottjwag@hotmail.com>
2025-04-14 14:23:06 -0600

    tryed to get the api to work

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -1,21 +1,26 @@
-﻿namespace WeatherApp
+﻿using System.Text.Json;
+using System.Net.Http;
+
+namespace WeatherApp
 {
     public partial class MainPage : ContentPage
     {
-        
+        private const string BaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+        private const string ApiKey = "f63648645d31e18e90750d381665a9a3";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnButtonClicked(object sender, EventArgs e)
+        private async void OnButtonClicked(object sender, EventArgs e)
         {
-            if (CityLabel.Text != "")
+            if (SearchEntry.Text != null) 
             {
                 CityLabel.Text = SearchEntry.Text;
             }
+            
         }
     }
-
+    
 }
